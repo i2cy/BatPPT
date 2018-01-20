4f12c2b2cab02177ee5e6ccc5ef484f19852b348
Icy Cloud <enderman1024@foxmail.com>
2018-01-20 15:46:29 +0800

    Add files via upload

---
+++ EXAMPLE/build release.bat
@@ -0,0 +1,22 @@
+@echo off
+path=%path%;"C:\Program Files\WinRAR"
+title File Builder
+if NOT EXIST release md release
+echo [BatPPT Release Builder] -by Icy
+echo 按任意键开始build
+set /a a=1
+pause >nul
+:start
+echo [第 %a% 次build]
+set /a a=%a%+1
+rar a pack.rar pack
+rd /s /q release
+md release
+echo Start...
+copy /b main.bat+pack.rar "release\batppt.bat"
+echo OK，release文件“batppt.bat”已生成至release文件夹下
+del pack.rar
+echo (按任意键重建)
+pause >nul
+cls
+goto start
---
+++ EXAMPLE/main.bat
@@ -0,0 +1,255 @@
+@echo off
+color 02
+title 生物实验报告 - 细胞器观察 [by Icy Cloud]
+echo 准备？
+echo [按任意键确认]
+pause >nul
+cls
+echo 设置环境...
+md pack
+ping -n 2 127.1 >nul
+path=%path%;"C:\Program Files\WinRAR";"pack"
+echo   完成
+echo 展开内嵌文件...
+ping -n 2 127.1 >nul
+copy "生物实验报告 - 细胞器观察[by Icy Cloud].bat" 02.bat
+ren 02.bat 02.rar
+unrar x 02.rar
+del 02.rar
+ping -n 2 127.1 >nul
+echo   完成
+ping -n 2 127.1 >nul
+cls
+echo 开始？
+echo [按任意键确认]
+pause >nul
+cls
+color 07
+echo ^>
+echo ^>
+echo ^>
+echo ^>
+echo ^>
+echo ^>
+echo ^>
+echo ^>
+echo ^>
+echo ^>
+echo ^>
+echo ^>
+echo ^>
+echo ^>
+echo ^>                                      ^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=
+echo ^>
+echo ^>                                         生      物      实      验      报      告
+echo ^>
+echo ^>                                      ^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=[by Icy Cloud]
+pause >nul
+cls
+echo ^>
+echo ^>
+echo ^>                                       项 目 I  -  观 察 活 水 藻 叶 面 细 胞 叶 绿 体
+echo ^>
+echo ^>
+echo ^>
+echo ^>       实验步骤：
+echo ^>         1.取一片较薄藻叶制成临时装片
+echo ^>         2.置于光学显微镜下用低倍镜观察并调焦至清晰
+echo ^>         3.将低倍镜切换为高倍镜仔细观察
+echo ^>         4.记录特征
+image ylt_x.bmp 500 100 >nul
+pause >nul
+image /d
+cls
+echo ^>
+echo ^>
+echo ^>                                       项 目 I  -  观 察 活 水 藻 叶 面 细 胞 叶 绿 体
+echo ^>
+echo ^>
+echo ^>
+echo ^>                                         高倍镜下：
+image ylt_d_s.bmp 500 100 >nul
+pause >nul
+image /d
+cls
+echo ^>
+echo ^>
+echo ^>                                       项 目 I  -  观 察 活 水 藻 叶 面 细 胞 叶 绿 体
+echo ^>
+echo ^>
+echo ^>
+echo ^>                                         高倍镜下：
+echo ^>
+echo ^>
+echo ^>
+echo ^>
+echo ^>
+echo ^>
+echo ^>
+echo ^>            可以观察到在靠近细胞壁处聚集有很多
+echo ^>        绿色球状物，大多呈扁球状，多聚集于细胞
+echo ^>        边缘（由于内部液泡排挤）
+image ylt_d.bmp 500 100 >nul
+pause >nul
+image /d
+cls
+echo ^>
+echo ^>
+echo ^>                                        项 目 II  -  观 察 口 腔 上 皮 细 胞 线 粒 体
+echo ^>
+echo ^>
+echo ^>
+echo ^>       实验步骤：
+echo ^>         1.用干净牙签在口腔上颚轻刮获得口腔上皮细胞
+echo ^>           放于临时装片上
+echo ^>         2.在其上滴一滴健那绿染色剂
+echo ^>         3.盖合玻片并用吸水纸吸去多余键那绿染色剂
+echo ^>         4.置于光学显微镜下用低倍镜观察并调焦至清晰
+echo ^>         5.将低倍镜切换为高倍镜仔细观察
+echo ^>         6.记录特征
+image xlt_x_s.bmp 500 100 >nul
+pause >nul
+cls
+echo ^>
+echo ^>
+echo ^>                                        项 目 II  -  观 察 口 腔 上 皮 细 胞 线 粒 体
+echo ^>
+echo ^>
+echo ^>
+echo ^>         那么，到底哪些才是我们要观察的对象呢？
+image xlt_x_s.bmp 500 100 >nul
+pause >nul
+cls
+image /d
+echo ^>
+echo ^>
+echo ^>                                        项 目 II  -  观 察 口 腔 上 皮 细 胞 线 粒 体
+echo ^>
+echo ^>
+echo ^>
+echo ^>         是这些？
+image xlt_x_q.bmp 500 100 >nul
+pause >nul
+cls
+echo ^>
+echo ^>
+echo ^>                                        项 目 II  -  观 察 口 腔 上 皮 细 胞 线 粒 体
+echo ^>
+echo ^>
+echo ^>
+echo ^>         显然不是
+image xlt_x_q.bmp 500 100 >nul
+pause >nul
+echo ^>
+echo ^>              这些圆滑得出奇的，外圈光滑，内圈由暗
+echo ^>         向中心变亮的东西是气泡，很容易分辨，去除
+echo ^>         它们只需在一旁用吸水纸吸去即可
+image xlt_x_q.bmp 500 100 >nul
+pause >nul
+cls
+image /d
+echo ^>
+echo ^>
+echo ^>                                        项 目 II  -  观 察 口 腔 上 皮 细 胞 线 粒 体
+echo ^>
+echo ^>
+echo ^>
+echo ^>         那，是这些么？
+image xlt_x_w.bmp 500 100 >nul
+pause >nul
+cls
+echo ^>
+echo ^>
+echo ^>                                        项 目 II  -  观 察 口 腔 上 皮 细 胞 线 粒 体
+echo ^>
+echo ^>
+echo ^>
+echo ^>         然而也不是
+image xlt_x_w.bmp 500 100 >nul
+pause >nul
+echo ^>
+echo ^>              这些东西看起来很突出，但大小不规则
+echo ^>         有的非常小有的很大一坨，形状不规则，完全
+echo ^>         看不出细胞结构。它们有可能是口腔里的赃物。
+echo ^>
+echo ^>         （好吧我承认当天早上没刷牙）
+image xlt_x_w.bmp 500 100 >nul
+pause >nul
+cls
+echo ^>
+echo ^>                                        项 目 II  -  观 察 口 腔 上 皮 细 胞 线 粒 体
+echo ^>
+echo ^>
+echo ^>
+echo ^>         这些，才是真正的口腔上皮细胞
+echo ^>
+echo ^>         （蛮多的不是么）
+image xlt_x.bmp 500 100 >nul
+pause >nul
+image /d
+cls
+echo ^>
+echo ^>                                        项 目 II  -  观 察 口 腔 上 皮 细 胞 线 粒 体
+echo ^>
+echo ^>
+echo ^>
+echo ^>                                         高倍镜下：
+image xlt_d_s.bmp 500 100 >nul
+pause >nul
+image /d
+cls
+echo ^>
+echo ^>                                        项 目 II  -  观 察 口 腔 上 皮 细 胞 线 粒 体
+echo ^>
+echo ^>
+echo ^>
+echo ^>                                         高倍镜下：
+echo ^>
+echo ^>
+echo ^>
+echo ^>
+echo ^>
+echo ^>
+echo ^>
+echo ^>            可以看到细胞内部有部分被染成了蓝色
+image xlt_d.bmp 500 100 >nul
+pause >nul
+echo ^>
+echo ^>         （但是我们真心不确定那到底是不是线粒体）
+image xlt_d.bmp 500 100 >nul
+pause >nul
+image /d
+cls
+echo ^>
+echo ^>
+echo ^>
+echo ^>
+echo ^>
+echo ^>
+echo ^>
+echo ^>
+echo ^>
+echo ^>
+echo ^>
+echo ^>
+echo ^>
+echo ^>
+echo ^>
+echo ^>
+echo ^>                                       [ E      N      D ]
+echo ^>
+echo ^>                                       ----------------------------------------
+echo ^>                                                           Thanks for Listening
+echo ^>
+echo ^>
+echo ^>
+echo ^>
+echo ^>
+echo ^>
+echo ^>
+echo ^>
+echo ^>
+echo ^>                                              （按任意键清除缓存并退出）
+pause >nul
+rd /s /q pack
+exit
